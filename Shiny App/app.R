6614028078ffcaec1df3a7be950ffb6d4080b79c
tybenz12 <tbenz1@macalester.edu>
2022-04-25 20:28:06 -0500

    added data wrangling

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -271,6 +271,103 @@ server <- function(input, output) {
   
   
   observeEvent(player_year(), {
+    FFData <- player_year() %>%
+      select(c(1:12), contains("ff")) %>%
+      add_column(Pitch_Type = "Four-Seam Fastball") %>%
+      mutate(pitch_frequency = n_ff_formatted, 
+             avg_speed = ff_avg_speed, 
+             avg_spin = ff_avg_spin, 
+             avg_break_x = ff_avg_break_x, 
+             avg_break_z = ff_avg_break_z, 
+             avg_break = ff_avg_break, 
+             range_speed = ff_range_speed) %>%
+      select(-contains("ff"))
+    
+    SliderData <- player_year() %>%
+      select(c(1:12), contains("sl")) %>%
+      add_column(Pitch_Type = "Slider") %>%
+      mutate(pitch_frequency = n_sl_formatted, 
+             avg_speed = sl_avg_speed, 
+             avg_spin = sl_avg_spin, 
+             avg_break_x = sl_avg_break_x, 
+             avg_break_z = sl_avg_break_z, 
+             avg_break = sl_avg_break, 
+             range_speed = sl_range_speed) %>%
+      select(-contains("sl"))
+    
+    ChangeData <- player_year() %>%
+      select(c(1:12), contains("ch")) %>%
+      add_column(Pitch_Type = "Changeup") %>%
+      mutate(pitch_frequency = n_ch_formatted, 
+             avg_speed = ch_avg_speed, 
+             avg_spin = ch_avg_spin, 
+             avg_break_x = ch_avg_break_x, 
+             avg_break_z = ch_avg_break_z, 
+             avg_break = ch_avg_break, 
+             range_speed = ch_range_speed) %>%
+      select(-c(13:19))
+    
+    CurveData <- player_year() %>%
+      select(c(1:12), contains("cu")) %>%
+      add_column(Pitch_Type = "Curveball") %>%
+      mutate(pitch_frequency = n_cukc_formatted, 
+             avg_speed = cu_avg_speed, 
+             avg_spin = cu_avg_spin, 
+             avg_break_x = cu_avg_break_x, 
+             avg_break_z = cu_avg_break_z, 
+             avg_break = cu_avg_break, 
+             range_speed = cu_range_speed) %>%
+      select(-contains("cu"))
+    
+    SinkerData <- player_year() %>%
+      select(c(1:12), contains("si")) %>%
+      add_column(Pitch_Type = "Sinker") %>%
+      mutate(pitch_frequency = n_sift_formatted, 
+             avg_speed = si_avg_speed, 
+             avg_spin = si_avg_spin, 
+             avg_break_x = si_avg_break_x, 
+             avg_break_z = si_avg_break_z, 
+             avg_break = si_avg_break, 
+             range_speed = si_range_speed) %>%
+      select(-contains("si"))
+    
+    CutterData <- player_year() %>%
+      select(c(1:12), contains("fc")) %>%
+      add_column(Pitch_Type = "Cutter") %>%
+      mutate(pitch_frequency = n_fc_formatted, 
+             avg_speed = fc_avg_speed, 
+             avg_spin = fc_avg_spin, 
+             avg_break_x = fc_avg_break_x, 
+             avg_break_z = fc_avg_break_z, 
+             avg_break = fc_avg_break, 
+             range_speed = fc_range_speed) %>%
+      select(-contains("fc"))
+    
+    SplitterData <- player_year() %>%
+      select(c(1:12), contains("fs")) %>%
+      add_column(Pitch_Type = "Splitter") %>%
+      mutate(pitch_frequency = n_fs_formatted, 
+             avg_speed = fs_avg_speed, 
+             avg_spin = fs_avg_spin, 
+             avg_break_x = fs_avg_break_x, 
+             avg_break_z = fs_avg_break_z, 
+             avg_break = fs_avg_break, 
+             range_speed = fs_range_speed) %>%
+      select(-contains("fs"))
+    
+    KnuckleData <- player_year() %>%
+      select(c(1:12), contains("kn")) %>%
+      add_column(Pitch_Type = "Knuckleball") %>%
+      mutate(pitch_frequency = n_kn_formatted, 
+             avg_speed = kn_avg_speed, 
+             avg_spin = kn_avg_spin, 
+             avg_break_x = kn_avg_break_x, 
+             avg_break_z = kn_avg_break_z, 
+             avg_break = kn_avg_break, 
+             range_speed = kn_range_speed) %>%
+      select(-contains("kn"))
+    
+    TidyPitchData <- bind_rows(FFData, SliderData, ChangeData, CurveData, SinkerData, CutterData, SplitterData, KnuckleData)
     if(input$Charts == "Average Speed by Pitch Type") {
       output$pitchChart <- renderPlot({
         #put pitch speed plot here
